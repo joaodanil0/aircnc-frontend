b6383c3145858c5386de4826404525b35cbbea2d
João <pintojr@icomp.ufam.edu.br>
2019-10-03 07:45:35 -0400

    Some organizations

--- src/App.js
+++ src/App.js
@@ -5,15 +5,13 @@ import logo from './assets/logo.svg';
 
 function App() {
   return (
-    <div className="container">
-      
-      <img src={logo} alt="AirCnC"/>
-      
+    <div className="container">      
+      <img src={logo} alt="AirCnC"/>      
       <div className="content">
         <p>
-          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
+          Ofereça <strong>spots</strong> para programadores e 
+          encontre <strong>talentos</strong> para sua empresa
         </p>
-
         <form>
           <label htmlFor="email">E-MAIL*</label>
           <input type="email" id="email" placeholder="Seu melhor email"/>
